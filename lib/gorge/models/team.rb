cfc26447f3bd55200dca989dac18226760bb13f7
Michael Senn <michael@senn.email>
2018-02-03 19:06:45 +0100

    Add specs for `Importer::RoundImporter`.

--- lib/gorge/models/team.rb
+++ lib/gorge/models/team.rb
@@ -4,5 +4,17 @@ module Gorge
   class Team < Sequel::Model
     one_to_many :rounds, key: :winning_team_id
     one_to_many :player_rounds
+
+    def self.draw
+      Team[0]
+    end
+
+    def self.marines
+      Team[1]
+    end
+
+    def self.aliens
+      Team[2]
+    end
   end
 end
